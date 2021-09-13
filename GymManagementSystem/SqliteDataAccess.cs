fa5364273c22ecb9b31bfda133beed69428fa931
BialekPL <mbialecki157@gmail.com>
2021-09-13 19:55:28 +0200

    próba nr 1 polaczenia z baza

---
+++ GymManagementSystem/SqliteDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SQLite;
+using System.Data;
+using Dapper;
+
+namespace GymManagementSystem
+{
+    class SqliteDataAccess
+    {
+
+        public static List<User> LoadUsers()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<User>("select * from UŻYTKOWNIK");
+                return output.ToList();
+            }
+        }
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
---
+++ GymManagementSystem/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementSystem
+{
+    class User
+    {
+        private long user_id;
+        private string pass_code ="pocz";
+        private string localization_id;
+        private string user_type;
+        private DateTime creation_date;
+        private DateTime pass_expire_date;
+        public User(long id_użytkownika, string kod_karnetu, string id_lokalizacji, string typ_użytkownika, DateTime data_utworzenia, DateTime data_wyg_karnetu)
+        {
+            user_id = id_użytkownika;
+            pass_code = kod_karnetu;
+            localization_id = id_lokalizacji;
+            user_type = typ_użytkownika;
+            creation_date = data_utworzenia;
+            pass_expire_date = data_wyg_karnetu;
+        }
+        
+//$exception	{"A parameterless default constructor or one matching signature
+   //         "(System.Int64 id_użytkownika, System.String kod_karnetu, System.String id_lokalizacji, System.String typ_użytkownika, System.DateTime data_utworzenia, System.DateTime data_wyg_karnetu)
+
+}
+}
